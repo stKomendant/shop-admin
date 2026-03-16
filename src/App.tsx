87fc5853feab9fd96bb8c8084b044a487e66794e
stKomendant <penifanta@gmail.com>
2026-03-16 19:12:01 +0000

    initial project setup with vite react typescript and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100 ">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Ecommerce Admin Dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default App;
